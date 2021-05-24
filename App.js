baa0bb0f25a1a47fae97236eb7ff8050f06cb318
iceyisaak <me@iceyisaak.com>
2021-05-24 13:02:20 +0200

    Debugging authUnsubscribe()

---
+++ App.js
@@ -0,0 +1,131 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { db } from './firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      lists: [],
+      uid: '',
+      loggedInText: 'Hi there!'
+    };
+
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const lists = [];
+    querySnapshot.forEach(
+      (doc) => {
+        var data = doc.data();
+        lists.push({
+          name: data.name,
+          items: data.items.toString()
+        });
+      }
+    );
+    this.setState({
+      lists
+    });
+  };
+
+  addList = () => {
+    this.referenceShoppingLists.add({
+      name: 'TestList',
+      items: ['egg', 'pasta', 'veggies']
+    });
+  };
+
+  componentDidMount() {
+
+    this.referenceShoppingLists = db.collection('shoppinglists');
+    this.unsubscribe = this.referenceShoppingLists.onSnapshot(this.onCollectionUpdate);
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(
+      async (user) => {
+        if (!user) {
+          await firebase.auth().signInAnonymously();
+        }
+
+        this.setState({
+          uid: user.uid,
+          loggedInText: 'You are logged in.'
+        });
+      }
+    );
+
+  };
+
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text style={styles.h1}>
+          All Shoppinglists
+        </Text>
+        <FlatList
+          data={this.state.lists}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Text style={styles.text}>
+              {item.name} : {item.items}
+            </Text>
+          )}
+        />
+        <Text>
+          {this.state.loggedInText}
+        </Text>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => this.addList()}
+        >
+          <Text style={styles.btnText}>
+            Add to List
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 40
+  },
+  item: {
+    fontSize: 20,
+    color: 'blue'
+  },
+  text: {
+    fontSize: 15,
+    color: 'blue'
+  },
+  h1: {
+    fontSize: 50,
+    marginBottom: 5
+  },
+  btn: {
+    backgroundColor: '#33dd00',
+    alignItems: 'center',
+    padding: 15
+  },
+  btnText: {
+    color: '#fff'
+  }
+});
---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqJpW4NzcX8z_trQBpuI6KLBNaG-HNfmg",
+  authDomain: "chatapp-7e1d5.firebaseapp.com",
+  projectId: "chatapp-7e1d5",
+  storageBucket: "chatapp-7e1d5.appspot.com",
+  messagingSenderId: "218328176238",
+  appId: "1:218328176238:web:4960ad1c7fb502ef61f87f"
+};
+
+// console.log('firebase: ', firebase);
+// if (!firebase.app.length) {
+firebase.initializeApp(firebaseConfig);
+// }
+
+export const db = firebase.firestore();
+
+export default firebase;
